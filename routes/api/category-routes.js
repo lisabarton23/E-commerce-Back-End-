8ba4e4e762d0734b3bdddb769835cf3fe2f9a044
Lisa Barton <lisabarton23@yahoo.com>
2021-05-26 14:42:11 -0600

    added router.get for CategoryData

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,7 +3,19 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  try { const CategoryData = await CategoryData.findAll({
+    include:[{ model : Product}],
+  });
+  res.status(200).json(CategoryData);}
+  catch (err){
+    res.status (500).json(err)
+  }
+
+
+
+
+  });
   // find all categories
   // be sure to include its associated Products
 });
