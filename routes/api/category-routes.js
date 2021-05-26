265c77d534953cd2c08d6d1302f09d159c911090
Lisa Barton <lisabarton23@yahoo.com>
2021-05-26 17:12:36 -0600

    added router.delete and router.put to categoryroutes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -40,8 +40,8 @@ router.post('/', async (req, res) => {
   // create a new category
   try{
 const locationData = await Category.create({
-  product_id: req.body.product_id,
-  //this product_id needs to be created so may not be correct
+  category_id: req.body.category_id,
+  
 });
 res.status(200).json(locationData);
   } catch (err) {
@@ -50,12 +50,36 @@ res.status(200).json(locationData);
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id',async (req, res) => {
+  try{
+    const locationData = await Category.create({
+      category_id: req.body.category_id,
   // update a category by its `id` value
 });
+res.status(200).json(locationData);
+  } catch (err) {
+    res.status(400).json(err);
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+  }
 });
+
+router.delete('/:id', async (req, res) => {
+  try{
+const CategoryData = await CategoryData.destroy({
+  where:{
+    id:req.params.id
+  },
+})
+if(!CategoryData){
+  res.status(404).json({ message: 'No category found with that id!' });
+  return;
+}
+
+res.status(200).json(CategoryData);
+} catch (err) {
+res.status(500).json(err);
+}
+});
+
 
 module.exports = router;
