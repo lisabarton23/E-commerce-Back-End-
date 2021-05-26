5ad5a97538efc2d2909d2c544b25809d29a371a7
Lisa Barton <lisabarton23@yahoo.com>
2021-05-26 16:00:12 -0600

    added foreignkeys to model index.js

--- models/index.js
+++ models/index.js
@@ -5,13 +5,24 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey:'category_id'
+})
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey:'category_id'
+  //do I need to add a CASCADE ON DELETE?
+})
 
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongtoMany(Tag,{
+  foreignKey:'ProductTag_id'
+  
+})
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, {
+  foreignKey:'ProductTag_id'
+})
 module.exports = {
   Product,
   Category,
